749f6b3eb1153e549f86508c17ceaa36b5d29953
Noah Paige <noah.paige@caci.com>
2024-12-11 17:56:15 -0800

    create UTCClock.vue

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import UTCClock from "./components/UTCClock.vue";
+import { useColorMode } from "@vueuse/core";
 
-<template>hello world</template>
+const mode = useColorMode();
+</script>
+
+<template>
+  <div class="w-screen h-screen flex flex-col justify-center items-center">
+    <UTCClock />
+  </div>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
-const animate = require("tailwindcss-animate")
+const animate = require("tailwindcss-animate");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
   safelist: ["dark"],
   prefix: "",
-  
+
   content: [
-    './pages/**/*.{ts,tsx,vue}',
-    './components/**/*.{ts,tsx,vue}',
-    './app/**/*.{ts,tsx,vue}',
-    './src/**/*.{ts,tsx,vue}',
-	],
-  
+    "./pages/**/*.{ts,tsx,vue}",
+    "./components/**/*.{ts,tsx,vue}",
+    "./app/**/*.{ts,tsx,vue}",
+    "./src/**/*.{ts,tsx,vue}",
+  ],
+
   theme: {
     container: {
       center: true,
@@ -74,11 +74,15 @@ module.exports = {
         },
         "collapsible-down": {
           from: { height: 0 },
-          to: { height: 'var(--radix-collapsible-content-height)' },
+          to: { height: "var(--radix-collapsible-content-height)" },
         },
         "collapsible-up": {
-          from: { height: 'var(--radix-collapsible-content-height)' },
+          from: { height: "var(--radix-collapsible-content-height)" },
           to: { height: 0 },
+        },
+        "flip-clock": {
+          from: { transform: "rotateX(-90deg)", opacity: "0.5" },
+          to: { transform: "rotateX(0deg)", opacity: "1" },
         },
       },
       animation: {
@@ -86,8 +90,9 @@ module.exports = {
         "accordion-up": "accordion-up 0.2s ease-out",
         "collapsible-down": "collapsible-down 0.2s ease-in-out",
         "collapsible-up": "collapsible-up 0.2s ease-in-out",
+        "flip-clock": "flip-clock 0.2s ease-out",
       },
     },
   },
   plugins: [animate],
-}
+};
