12434dd1fc94089cbda1e2280fb4134abf8cf5da
Noah Paige <noah.paige@caci.com>
2024-12-18 17:17:03 -0800

    orgainize clocks layout

--- src/components/Pages/AnotherPage.vue
+++ src/components/Pages/AnotherPage.vue
@@ -17,19 +17,29 @@ useIntervalFn(() => {
 
 <template>
   <div class="w-screen h-screen flex p-5 justify-center items-center">
-    <div class="w-screen h-fit flex flex-row flex-wrap gap-8 justify-center items-center">
-      <Clock labelRight="UTC" :time="clockData.utc" size="2xl" />
-      <Clock :labelRight="clockData.timezoneStr" :time="clockData.local" size="2xl" />
-      <Clock :labelLeft="clockData.t > 0 ? 'T+' : 'T-'" :time="clockData.t" timeType="timespan" size="xl" />
-      <Clock :labelLeft="clockData.l > 0 ? 'L+' : 'L-'" :time="clockData.l" timeType="timespan" size="xl" />
-      <Clock :time="clockData.tZero" labelTop="T-Zero" labelRight="UTC" timeType="date" format="DDHHMMSS" size="xl" />
-      <Clock :time="clockData.holdRemaining" labelTop="Hold Remaining" timeType="timespan" size="lg" />
-      <Clock :time="clockData.untilRestart" labelTop="Time Until Restart" timeType="timespan" size="lg" />
-      <Clock :time="clockData.windowRemaining" labelTop="Window Remaining" timeType="timespan" size="lg" />
-      <Clock :time="clockData.windowUsed" labelTop="Window Used" timeType="timespan" size="lg" />
-      <Clock :time="clockData.met" labelLeft="MET" timeType="timespan" size="md" />
-      <Clock :time="clockData.met" labelLeft="MET" timeType="timespan" format="MMSS" size="md" />
-      <Clock :time="clockData.met" labelLeft="MET" timeType="timespan" format="SS" size="md" />
+    <div class="flex flex-col gap-8 justify-center items-center">
+      <div class="flex flex-row flex-wrap gap-8 justify-center items-center">
+        <Clock labelRight="UTC" :time="clockData.utc" size="2xl" />
+        <Clock :labelRight="clockData.timezoneStr" :time="clockData.local" size="2xl" />
+      </div>
+      <div class="flex flex-row flex-wrap gap-8 justify-center items-center">
+        <Clock :labelLeft="clockData.t > 0 ? 'T+' : 'T-'" :time="clockData.t" timeType="timespan" size="xl" />
+        <Clock :labelLeft="clockData.l > 0 ? 'L+' : 'L-'" :time="clockData.l" timeType="timespan" size="xl" />
+      </div>
+      <div class="flex flex-row flex-wrap gap-8 justify-center items-center">
+        <Clock :time="clockData.tZero" labelTop="T-Zero" labelRight="UTC" timeType="date" format="DDHHMMSS" size="xl" />
+      </div>
+      <div class="flex flex-row flex-wrap gap-8 justify-center items-center">
+        <Clock :time="clockData.holdRemaining" labelTop="Hold Remaining" timeType="timespan" size="lg" />
+        <Clock :time="clockData.untilRestart" labelTop="Time Until Restart" timeType="timespan" size="lg" />
+        <Clock :time="clockData.windowRemaining" labelTop="Window Remaining" timeType="timespan" size="lg" />
+        <Clock :time="clockData.windowUsed" labelTop="Window Used" timeType="timespan" size="lg" />
+      </div>
+      <div class="flex flex-row flex-wrap gap-8 justify-center items-center">
+        <Clock :time="clockData.met" labelLeft="MET" timeType="timespan" size="md" />
+        <Clock :time="clockData.met" labelLeft="MET" timeType="timespan" format="MMSS" size="md" />
+        <Clock :time="clockData.met" labelLeft="MET" timeType="timespan" format="SS" size="md" />
+      </div>
     </div>
   </div>
 </template>
