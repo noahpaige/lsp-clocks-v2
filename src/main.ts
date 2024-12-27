c844a4c641f526cc055c58dc8fce1c2e65171acc
Noah Paige <noah.paige@caci.com>
2024-12-27 13:29:39 -0800

    add redis package

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { createApp } from "vue";
 import "./assets/index.css";
 import App from "./App.vue";
+import { createClient } from "redis";
+
+const port = 6879;
+const client = createClient({
+  url: `redis://localhost:${port}`,
+});
+
+client.on("error", (err) => console.log("Redis Client Error", err));
+
+await client.connect();
 
 // TODO
 // start up redis client and listen to clock events
