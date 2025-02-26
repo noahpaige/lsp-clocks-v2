2059e7e654fa26c4fadec1e46693843c3c854d05
Noah Paige <noah.paige@caci.com>
2025-02-26 13:23:41 -0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
 import { useColorMode } from "@vueuse/core";
-import { useRoute } from "vue-router";
 import { Toaster } from "@/components/ui/sonner";
-import notificationManager from "@/logic/NotificationManager";
+import NotificationManager from "@/logic/NotificationManager";
 import { onUnmounted } from "vue";
 
-const notificationMan = new notificationManager();
+const notificationMan = NotificationManager.getInstance();
 
 // need this to enable dark mode
 const mode = useColorMode();
