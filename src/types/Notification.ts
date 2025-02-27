c73f6f203b2abb5fad392c92e05e3702a571988c
Noah Paige <noah.paige@caci.com>
2025-02-26 23:40:02 -0800

    Update Notification.ts

--- src/types/Notification.ts
+++ src/types/Notification.ts
@@ -1,7 +1,7 @@
 export interface Notification {
   title: string;
   description: string;
-  duration?: number;
+  duration?: number | "infinite";
   severity?: "success" | "info" | "warning" | "error"; // defaults to info
   deliverTo?: "displays" | "home" | "all"; // defaults to home
 }
