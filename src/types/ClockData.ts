78024a38f184f4bedf6ccd4ccfdce20d00b787dc
Noah Paige <noahlandonpaige@gmail.com>
2025-03-21 15:56:40 -0700

    Create ClockData.ts

---
+++ src/types/ClockData.ts
@@ -0,0 +1,13 @@
+export interface ClockDataType {
+  utc: number;
+  local: number;
+  timezoneStr: string;
+  t: number;
+  l: number;
+  holdRemaining: number;
+  untilRestart: number;
+  windowUsed: number;
+  windowRemaining: number;
+  tZero: number;
+  met: number;
+}
