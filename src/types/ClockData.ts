6a620ae73bc91b3d768b8be917523871b6e6e722
Noah Paige <noah.paige@caci.com>
2025-03-26 19:02:53 -0700

    add parser for clockdata type

--- src/types/ClockData.ts
+++ src/types/ClockData.ts
@@ -11,3 +11,19 @@ export interface ClockDataType {
   tZero: number;
   met: number;
 }
+
+export function parseClockData(raw: any): ClockDataType {
+  return {
+    utc: Number(raw.utc),
+    local: Number(raw.local),
+    timezoneStr: raw.timezoneStr,
+    t: Number(raw.t),
+    l: Number(raw.l),
+    holdRemaining: Number(raw.holdRemaining),
+    untilRestart: Number(raw.untilRestart),
+    windowUsed: Number(raw.windowUsed),
+    windowRemaining: Number(raw.windowRemaining),
+    tZero: Number(raw.tZero),
+    met: Number(raw.met),
+  };
+}
