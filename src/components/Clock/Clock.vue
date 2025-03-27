76de844687b20dccf321a1d9ca2787f47cb0e29d
Noah Paige <noah.paige@caci.com>
2025-03-26 19:12:05 -0700

    formatting for clock.vue

--- src/components/Clock/Clock.vue
+++ src/components/Clock/Clock.vue
@@ -1,112 +1,149 @@
 <template>
   <div class="flex align-middle w-fit h-fit justify-center items-center transition-all">
-    <div :data-size="props.size" :class="[
-      'flex flex-col bg-slate-800/[0.6] backdrop-blur-lg rounded-lg',
-      'data-[size=xs]:text-[1rem]   data-[size=xs]:p-2',
-      'data-[size=sm]:text-[2rem]   data-[size=sm]:p-3',
-      'data-[size=md]:text-[3rem]   data-[size=md]:p-4',
-      'data-[size=lg]:text-[5rem]   data-[size=lg]:p-6',
-      'data-[size=xl]:text-[8rem]   data-[size=xl]:p-8',
-      'data-[size=2xl]:text-[10rem] data-[size=2xl]:p-10',
-    ]">
-      <div v-if="labelTop !== ''" :data-size="props.size" :class="[
-        'text-muted-foreground font-medium',
-        'data-[size=xs]:text-[0.5625rem] data-[size=xs]:mt-[-0.3125rem]',
-        'data-[size=sm]:text-[1.125rem]  data-[size=sm]:mt-[-0.5625rem]',
-        'data-[size=md]:text-[1.625rem]  data-[size=md]:mt-[-0.8125rem]',
-        'data-[size=lg]:text-[2.125rem]  data-[size=lg]:mt-[-1.0625rem]',
-        'data-[size=xl]:text-[3.125rem]  data-[size=xl]:mt-[-1.5rem]',
-        'data-[size=2xl]:text-[4.625rem] data-[size=2xl]:mt-[-2.3125rem]',
-      ]">
+    <div
+      :data-size="props.size"
+      :class="[
+        'flex flex-col bg-slate-800/[0.6] backdrop-blur-lg rounded-lg',
+        'data-[size=xs]:text-[1rem]   data-[size=xs]:p-2',
+        'data-[size=sm]:text-[2rem]   data-[size=sm]:p-3',
+        'data-[size=md]:text-[3rem]   data-[size=md]:p-4',
+        'data-[size=lg]:text-[5rem]   data-[size=lg]:p-6',
+        'data-[size=xl]:text-[8rem]   data-[size=xl]:p-8',
+        'data-[size=2xl]:text-[10rem] data-[size=2xl]:p-10',
+      ]"
+    >
+      <div
+        v-if="labelTop !== ''"
+        :data-size="props.size"
+        :class="[
+          'text-muted-foreground font-medium',
+          'data-[size=xs]:text-[0.5625rem] data-[size=xs]:mt-[-0.3125rem]',
+          'data-[size=sm]:text-[1.125rem]  data-[size=sm]:mt-[-0.5625rem]',
+          'data-[size=md]:text-[1.625rem]  data-[size=md]:mt-[-0.8125rem]',
+          'data-[size=lg]:text-[2.125rem]  data-[size=lg]:mt-[-1.0625rem]',
+          'data-[size=xl]:text-[3.125rem]  data-[size=xl]:mt-[-1.5rem]',
+          'data-[size=2xl]:text-[4.625rem] data-[size=2xl]:mt-[-2.3125rem]',
+        ]"
+      >
         {{ labelTop }}
       </div>
-      <div :data-size="props.size" :class="[
-        'flex align-middle justify-center items-center select-none',
-        'data-[size=xs]:gap-0.5',
-        'data-[size=sm]:gap-1',
-        'data-[size=md]:gap-2',
-        'data-[size=lg]:gap-3',
-        'data-[size=xl]:gap-4',
-        'data-[size=2xl]:gap-5',
-      ]">
-        <div v-if="labelLeft !== ''" :data-size="props.size" :class="[
-          'text-muted-foreground font-thin leading-none',
-          'data-[size=xs]:mr-1',
-          'data-[size=sm]:mr-1',
-          'data-[size=md]:mr-3',
-          'data-[size=lg]:mr-5',
-          'data-[size=xl]:mr-8',
-          'data-[size=2xl]:mr-10',
-        ]">
+      <div
+        :data-size="props.size"
+        :class="[
+          'flex align-middle justify-center items-center select-none',
+          'data-[size=xs]:gap-0.5',
+          'data-[size=sm]:gap-1',
+          'data-[size=md]:gap-2',
+          'data-[size=lg]:gap-3',
+          'data-[size=xl]:gap-4',
+          'data-[size=2xl]:gap-5',
+        ]"
+      >
+        <div
+          v-if="labelLeft !== ''"
+          :data-size="props.size"
+          :class="[
+            'text-muted-foreground font-thin leading-none',
+            'data-[size=xs]:mr-1',
+            'data-[size=sm]:mr-1',
+            'data-[size=md]:mr-3',
+            'data-[size=lg]:mr-5',
+            'data-[size=xl]:mr-8',
+            'data-[size=2xl]:mr-10',
+          ]"
+        >
           {{ labelLeft }}
         </div>
-        <div v-if="timeArr[0][0] !== undefined" :data-size="props.size" :key="lengthArr[0]" :class="[
-          'flex flex-col items-center overflow-y-hidden leading-none',
-          `w-[${lengthArr[0]}ch]`,
-          'data-[size=xs]:h-[1rem] data-[size=xs]:leading-[1rem]     data-[size=xs]:mr-[0.75rem]',
-          'data-[size=sm]:h-[2rem] data-[size=sm]:leading-[2rem]     data-[size=sm]:mr-[1rem]',
-          'data-[size=md]:h-[3rem] data-[size=md]:leading-[3rem]     data-[size=md]:mr-[1.5rem]',
-          'data-[size=lg]:h-[5rem] data-[size=lg]:leading-[5rem]     data-[size=lg]:mr-[2rem]',
-          'data-[size=xl]:h-[8rem] data-[size=xl]:leading-[8rem]     data-[size=xl]:mr-[3rem]',
-          'data-[size=2xl]:h-[10rem] data-[size=2xl]:leading-[10rem] data-[size=2xl]:mr-[4rem]',
-        ]">
-          <div class="animate-flip-clock " v-for="(timeStr, index) in timeArr[0]" :key="`${index}-${timeStr}`">
+        <div
+          v-if="timeArr[0][0] !== undefined"
+          :data-size="props.size"
+          :key="lengthArr[0]"
+          :class="[
+            'flex flex-col items-center overflow-y-hidden leading-none',
+            `w-[${lengthArr[0]}ch]`,
+            'data-[size=xs]:h-[1rem] data-[size=xs]:leading-[1rem]     data-[size=xs]:mr-[0.75rem]',
+            'data-[size=sm]:h-[2rem] data-[size=sm]:leading-[2rem]     data-[size=sm]:mr-[1rem]',
+            'data-[size=md]:h-[3rem] data-[size=md]:leading-[3rem]     data-[size=md]:mr-[1.5rem]',
+            'data-[size=lg]:h-[5rem] data-[size=lg]:leading-[5rem]     data-[size=lg]:mr-[2rem]',
+            'data-[size=xl]:h-[8rem] data-[size=xl]:leading-[8rem]     data-[size=xl]:mr-[3rem]',
+            'data-[size=2xl]:h-[10rem] data-[size=2xl]:leading-[10rem] data-[size=2xl]:mr-[4rem]',
+          ]"
+        >
+          <div class="animate-flip-clock" v-for="(timeStr, index) in timeArr[0]" :key="`${index}-${timeStr}`">
             {{ timeStr }}
           </div>
         </div>
-        <div v-if="timeArr[1][0] !== undefined" :data-size="props.size" :key="lengthArr[1]" :class="[
-          'flex flex-col items-center overflow-y-hidden leading-none',
-          `w-[${lengthArr[1]}ch]`,
-          'data-[size=xs]:h-[1rem]   data-[size=xs]:leading-[1rem]',
-          'data-[size=sm]:h-[2rem]   data-[size=sm]:leading-[2rem]',
-          'data-[size=md]:h-[3rem]   data-[size=md]:leading-[3rem]',
-          'data-[size=lg]:h-[5rem]   data-[size=lg]:leading-[5rem]',
-          'data-[size=xl]:h-[8rem]   data-[size=xl]:leading-[8rem]',
-          'data-[size=2xl]:h-[10rem] data-[size=2xl]:leading-[10rem]',
-        ]">
+        <div
+          v-if="timeArr[1][0] !== undefined"
+          :data-size="props.size"
+          :key="lengthArr[1]"
+          :class="[
+            'flex flex-col items-center overflow-y-hidden leading-none',
+            `w-[${lengthArr[1]}ch]`,
+            'data-[size=xs]:h-[1rem]   data-[size=xs]:leading-[1rem]',
+            'data-[size=sm]:h-[2rem]   data-[size=sm]:leading-[2rem]',
+            'data-[size=md]:h-[3rem]   data-[size=md]:leading-[3rem]',
+            'data-[size=lg]:h-[5rem]   data-[size=lg]:leading-[5rem]',
+            'data-[size=xl]:h-[8rem]   data-[size=xl]:leading-[8rem]',
+            'data-[size=2xl]:h-[10rem] data-[size=2xl]:leading-[10rem]',
+          ]"
+        >
           <div class="animate-flip-clock" v-for="(timeStr, index) in timeArr[1]" :key="`${index}-${timeStr}`">
             {{ timeStr }}
           </div>
         </div>
         <div v-if="timeArr[1][0] !== undefined" class="text-muted-foreground font-thin leading-none">:</div>
-        <div v-if="timeArr[2][0] !== undefined" :data-size="props.size" :key="lengthArr[2]" :class="[
-          'flex flex-col items-center overflow-y-hidden leading-none',
-          `w-[${lengthArr[2]}ch]`,
-          'data-[size=xs]:h-[1rem]   data-[size=xs]:leading-[1rem]',
-          'data-[size=sm]:h-[2rem]   data-[size=sm]:leading-[2rem]',
-          'data-[size=md]:h-[3rem]   data-[size=md]:leading-[3rem]',
-          'data-[size=lg]:h-[5rem]   data-[size=lg]:leading-[5rem]',
-          'data-[size=xl]:h-[8rem]   data-[size=xl]:leading-[8rem]',
-          'data-[size=2xl]:h-[10rem] data-[size=2xl]:leading-[10rem]',
-        ]">
-          <div class="animate-flip-clock " v-for="(timeStr, index) in timeArr[2]" :key="`${index}-${timeStr}`">
+        <div
+          v-if="timeArr[2][0] !== undefined"
+          :data-size="props.size"
+          :key="lengthArr[2]"
+          :class="[
+            'flex flex-col items-center overflow-y-hidden leading-none',
+            `w-[${lengthArr[2]}ch]`,
+            'data-[size=xs]:h-[1rem]   data-[size=xs]:leading-[1rem]',
+            'data-[size=sm]:h-[2rem]   data-[size=sm]:leading-[2rem]',
+            'data-[size=md]:h-[3rem]   data-[size=md]:leading-[3rem]',
+            'data-[size=lg]:h-[5rem]   data-[size=lg]:leading-[5rem]',
+            'data-[size=xl]:h-[8rem]   data-[size=xl]:leading-[8rem]',
+            'data-[size=2xl]:h-[10rem] data-[size=2xl]:leading-[10rem]',
+          ]"
+        >
+          <div class="animate-flip-clock" v-for="(timeStr, index) in timeArr[2]" :key="`${index}-${timeStr}`">
             {{ timeStr }}
           </div>
         </div>
         <div v-if="timeArr[2][0] !== undefined" class="text-muted-foreground font-thin leading-none">:</div>
-        <div :data-size="props.size" :key="lengthArr[3]" :class="[
-          'flex flex-col items-center overflow-y-hidden leading-none',
-          `w-[${lengthArr[3]}ch]`,
-          'data-[size=xs]:h-[1rem]   data-[size=xs]:leading-[1rem]',
-          'data-[size=sm]:h-[2rem]   data-[size=sm]:leading-[2rem]',
-          'data-[size=md]:h-[3rem]   data-[size=md]:leading-[3rem]',
-          'data-[size=lg]:h-[5rem]   data-[size=lg]:leading-[5rem]',
-          'data-[size=xl]:h-[8rem]   data-[size=xl]:leading-[8rem]',
-          'data-[size=2xl]:h-[10rem] data-[size=2xl]:leading-[10rem]',
-        ]">
-          <div class=" animate-flip-clock" v-for="(timeStr, index) in timeArr[3]" :key="`${index}-${timeStr}`">
+        <div
+          :data-size="props.size"
+          :key="lengthArr[3]"
+          :class="[
+            'flex flex-col items-center overflow-y-hidden leading-none',
+            `w-[${lengthArr[3]}ch]`,
+            'data-[size=xs]:h-[1rem]   data-[size=xs]:leading-[1rem]',
+            'data-[size=sm]:h-[2rem]   data-[size=sm]:leading-[2rem]',
+            'data-[size=md]:h-[3rem]   data-[size=md]:leading-[3rem]',
+            'data-[size=lg]:h-[5rem]   data-[size=lg]:leading-[5rem]',
+            'data-[size=xl]:h-[8rem]   data-[size=xl]:leading-[8rem]',
+            'data-[size=2xl]:h-[10rem] data-[size=2xl]:leading-[10rem]',
+          ]"
+        >
+          <div class="animate-flip-clock" v-for="(timeStr, index) in timeArr[3]" :key="`${index}-${timeStr}`">
             {{ timeStr }}
           </div>
         </div>
-        <div v-if="labelRight !== ''" :data-size="props.size" :class="[
-          'text-muted-foreground font-thin leading-none',
-          'data-[size=xs]:ml-1',
-          'data-[size=sm]:ml-1',
-          'data-[size=md]:ml-3',
-          'data-[size=lg]:ml-5',
-          'data-[size=xl]:ml-8',
-          'data-[size=2xl]:ml-10',
-        ]">
+        <div
+          v-if="labelRight !== ''"
+          :data-size="props.size"
+          :class="[
+            'text-muted-foreground font-thin leading-none',
+            'data-[size=xs]:ml-1',
+            'data-[size=sm]:ml-1',
+            'data-[size=md]:ml-3',
+            'data-[size=lg]:ml-5',
+            'data-[size=xl]:ml-8',
+            'data-[size=2xl]:ml-10',
+          ]"
+        >
           {{ labelRight }}
         </div>
       </div>
@@ -169,7 +206,6 @@ watch(
     updateClock();
   }
 );
-
 
 function updateClock() {
   let timeStrs = timeToArr(props.time, props.timeType, props.format);
