8d6f28d72d620abf5aa12e2efce81ac36276f342
Noah Paige <noah.paige@caci.com>
2025-05-12 14:09:49 -0700

    remove files for bare-bones branch

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,9 @@
 import { useColorMode } from "@vueuse/core";
 import { Toaster } from "@/components/ui/sonner";
 import { useToaster } from "@/composables/useToaster";
-import { useWindowManager } from "@/composables/WindowManager/useWindowManager";
 // need this to enable dark mode
 const mode = useColorMode();
 const toaster = useToaster();
-const windowMan = useWindowManager();
 </script>
 
 <template>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,16 +1,7 @@
-import HomePage from "@/components/Pages/HomePage/HomePage.vue";
-import ClocksDemoPage from "@/components/Pages/Displays/ClocksDemoPage.vue";
-import AllClocks from "@/components/Pages/Displays/AllClocks.vue";
-import TimezoneClock from "@/components/Pages/Displays/TimezoneClock.vue";
 import LoginPage from "@/components/Pages/LoginPage/LoginPage.vue";
 import RegisterPage from "@/components/Pages/RegisterPage/RegisterPage.vue";
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: HomePage,
-  },
   {
     path: "/login",
     name: "Login",
@@ -20,21 +11,6 @@ const routes = [
     path: "/register",
     name: "Register",
     component: RegisterPage,
-  },
-  {
-    path: "/displays/clocks-demo",
-    name: "Clocks Demo",
-    component: ClocksDemoPage,
-  },
-  {
-    path: "/displays/all-clocks-new",
-    name: "All Clocks",
-    component: AllClocks,
-  },
-  {
-    path: "/displays/timezone-clocks-new",
-    name: "Timezone Clocks",
-    component: TimezoneClock,
   },
 
   // Add more routes as needed
