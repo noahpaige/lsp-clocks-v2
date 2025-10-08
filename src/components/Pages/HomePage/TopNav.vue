c0419ce8fe191ce7772d7879635a7567516de872
Noah Paige <noah.paige@caci.com>
2025-10-08 12:12:04 -0700

    topnav edits

--- src/components/Pages/HomePage/TopNav.vue
+++ src/components/Pages/HomePage/TopNav.vue
@@ -1,7 +1,40 @@
 <script setup>
+import { useRoute } from "vue-router";
 import { Button } from "@/components/ui/button";
 import { Settings, Clock } from "lucide-vue-next";
 import { useToaster } from "@/composables/useToaster";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { Separator } from "@/components/ui/separator";
+
+const route = useRoute();
+
+const navItems = {
+  home: {
+    name: "Home",
+    href: "/",
+  },
+  displayController: {
+    name: "Display Controller",
+    href: "/display-controller",
+  },
+  configuration: {
+    name: "Configuration",
+    href: "/config",
+  },
+};
+
+// Store the href of the currently selected nav item (set once on component load)
+const selectedHref = route.path;
 
 const { emitToast } = useToaster();
 
@@ -16,10 +49,31 @@ const onClickSettings = () => {
 
 <template>
   <div class="flex items-center justify-between border-b p-2">
-    <div class="flex items-center">
-      <Clock class="bg-primary text-black rounded-full w-5 h-5" />
-      <div class="text-lg font-bold ml-2">LSP Clocks</div>
+    <div class="flex items-center gap-4">
+      <div class="flex items-center">
+        <Clock class="bg-slate-300 text-black rounded-full w-8 h-8 mt-1" />
+        <div class="text-4xl text-slate-300 font-bold ml-2">LSP Clocks</div>
+      </div>
+      <Separator orientation="vertical" class="h-14 -my-6" />
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem v-for="(item, key) in navItems" :key="key">
+            <NavigationMenuLink
+              :href="item.href"
+              :class="[
+                navigationMenuTriggerStyle(),
+                selectedHref === item.href ? 'bg-accent/50 text-accent-foreground scale-105' : 'text-foreground/60',
+              ]"
+            >
+              {{ item.name }}
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
     </div>
-    <Button variant="outline" @click="onClickSettings" q><Settings /> </Button>
+    <div class="flex items-center">
+      <Separator orientation="vertical" class="h-14 -my-6 mr-2" />
+      <Button variant="ghost" @click="onClickSettings"><Settings /> </Button>
+    </div>
   </div>
 </template>
