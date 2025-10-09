31c5d40cc1bda387cb6096c4ad03148c229ef5a3
Noah Paige <noah.paige@caci.com>
2025-10-09 15:32:16 -0700

    Update SidebarTrigger.vue

--- src/components/ui/sidebar/SidebarTrigger.vue
+++ src/components/ui/sidebar/SidebarTrigger.vue
@@ -1,15 +1,34 @@
 <script setup lang="ts">
-import type { HTMLAttributes } from "vue"
-import { PanelLeft } from "lucide-vue-next"
-import { cn } from "@/lib/utils"
-import { Button } from '@/components/ui/button'
-import { useSidebar } from "./utils"
+import type { HTMLAttributes } from "vue";
+import { computed } from "vue";
+import { PanelLeft } from "lucide-vue-next";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { useSidebar } from "./utils";
 
-const props = defineProps<{
-  class?: HTMLAttributes["class"]
-}>()
+const props = withDefaults(
+  defineProps<{
+    class?: HTMLAttributes["class"];
+    size?: "xs" | "sm" | "md" | "lg" | "xl" | "2xl";
+  }>(),
+  {
+    size: "sm",
+  }
+);
 
-const { toggleSidebar } = useSidebar()
+const { toggleSidebar } = useSidebar();
+
+const sizeClasses = computed(() => {
+  const sizes = {
+    xs: { button: "h-6 w-6 [&_svg]:!size-3.5", icon: 14 },
+    sm: { button: "h-7 w-7 [&_svg]:!size-4", icon: 16 },
+    md: { button: "h-9 w-9 [&_svg]:!size-5", icon: 20 },
+    lg: { button: "h-10 w-10 [&_svg]:!size-6", icon: 24 },
+    xl: { button: "h-12 w-12 [&_svg]:!size-7", icon: 28 },
+    "2xl": { button: "h-14 w-14 [&_svg]:!size-8", icon: 32 },
+  };
+  return sizes[props.size];
+});
 </script>
 
 <template>
@@ -17,10 +36,10 @@ const { toggleSidebar } = useSidebar()
     data-sidebar="trigger"
     variant="ghost"
     size="icon"
-    :class="cn('h-7 w-7', props.class)"
+    :class="cn(sizeClasses.button, props.class)"
     @click="toggleSidebar"
   >
-    <PanelLeft />
+    <PanelLeft :size="sizeClasses.icon" />
     <span class="sr-only">Toggle Sidebar</span>
   </Button>
 </template>
